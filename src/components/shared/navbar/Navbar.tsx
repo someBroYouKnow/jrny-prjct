f6242f7b665b48b496820c76f6d65c3fa5b3392d
someBroYouKnow <https://github.com/someBroYouKnow>
2025-04-03 18:43:11 +0530

    fixed typo

--- src/components/shared/navbar/Navbar.tsx
+++ src/components/shared/navbar/Navbar.tsx
@@ -8,7 +8,7 @@ export const NavBar = ()=>{
           <li> <Link to='/'>Home</Link></li>
           <li> <Link to='/portfolio'>Portfolio</Link></li>
           <li> <Link to='/about-us'>About Us</Link></li>
-          <li> <Link to='/blogs'>Blogs</Link></li>
+          <li> <Link to='/blog'>Blogs</Link></li>
           <li> <Link to='/contact-us'>Contact</Link></li>
         </ul>
       </nav>
